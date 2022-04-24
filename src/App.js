00aff142bd5f6ddfce209a0475bcfcd80723920d
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-04-24 11:47:14 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './App.css';
+// import culinary from './Images/culinaryd.jpg';
+// import restraunt from './Images/restrauntk.jpg';
+// import wine from './Images/wine5.JPG';
+
+function App(){
+ 
+  return (
+    <div className='App'>
+       
+        <div className='kashee info'>
+          <span>W</span>
+          <span>e</span>
+          <span>l</span>
+          <span>c</span>
+          <span>o</span>
+          <span>m</span>
+          <span>e</span>
+          <span>!</span>
+        </div>
+       <div className='kashee info_1'>
+          <span>t</span>
+          <span>o</span>
+          <span>m</span>
+          <span>y</span>
+          </div>
+          <div className='kashee info_2'>
+          <span>c</span>
+          <span>r</span>
+          <span>e</span>
+          <span>a</span>
+          <span>t</span>
+          <span>i</span>
+          <span>v</span>
+          <span>e</span>
+          <span>s</span>
+          <span>p</span>
+          <span>a</span>
+          <span>c</span>
+          <span>e</span>
+       </div>
+    </div>
+    
+  )
+}
+
+export default App;
